e5f099cce13e0e441ee62fe827295559a15d16b2
DESKTOP-G7VMHJI\Alejandro <alexcpesp@gmail.com>
2022-04-28 16:56:18 +0200

    added more examples, and not much else

--- kyosho/defines.h
+++ kyosho/defines.h
@@ -1,1 +1,9 @@
-//aqui podemos definir toda variable global o cualquier cosa que queramos hacer
+#ifndef DEFINES
+#define DEFINES
+#include "Arduino.h"
+//#include "pwm_read.h"
+
+
+#define PWM_READ_PIN 13
+#endif
+
--- kyosho/kyosho.ino
+++ kyosho/kyosho.ino
@@ -1,9 +1,11 @@
-#include "Arduino.h"
 #include "defines.h"
+//#define PWM_READ_PIN 13
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+	int a=PWM_READ_PIN;
+	//pwm_reader distance_ref(PWM_READ_PIN);
 }
 
 // The loop function is called in an endless loop
