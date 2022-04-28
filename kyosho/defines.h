161df68e680ca98234981fe47ab7c3f2c18e068e
DESKTOP-G7VMHJI\Alejandro <alexcpesp@gmail.com>
2022-04-28 18:39:25 +0200

    primeros pasos

--- kyosho/defines.h
+++ kyosho/defines.h
@@ -1,9 +1,13 @@
 #ifndef DEFINES
 #define DEFINES
 #include "Arduino.h"
-//#include "pwm_read.h"
+#include "driver/mcpwm.h"
+
+
 
 
 #define PWM_READ_PIN 13
+
+void reader(void *pvParameters);
 #endif
 
--- kyosho/kyosho.ino
+++ kyosho/kyosho.ino
@@ -1,15 +1,37 @@
 #include "defines.h"
-//#define PWM_READ_PIN 13
+
+unsigned int pwm;
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-	int a=PWM_READ_PIN;
-	//pwm_reader distance_ref(PWM_READ_PIN);
+	Serial.begin(115200); // to read serial
+	/*
+	 * you can read PWM like this
+	 * https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/mcpwm.html
+	 * */
+	mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM_CAP_0, PWM_READ_PIN);
+	xTaskCreatePinnedToCore(reader, "PWM_reader", 2048, NULL, 1, NULL, 1);
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+	vTaskDelay(100000);
+
+}
+
+
+void reader(void *pvParameters) {
+	int32_t pos_edge, neg_edge;
+	while(true){
+		mcpwm_capture_enable(MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_NEG_EDGE, 0);
+		neg_edge = mcpwm_capture_signal_get_value(MCPWM_UNIT_0, MCPWM_SELECT_CAP0);
+		Serial.println(neg_edge);
+		mcpwm_capture_enable(MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_POS_EDGE, 0);
+		pos_edge = mcpwm_capture_signal_get_value(MCPWM_UNIT_0, MCPWM_SELECT_CAP0);
+		Serial.println(pos_edge);
+		pwm=1000000+(pos_edge-neg_edge);
+	}
 }
