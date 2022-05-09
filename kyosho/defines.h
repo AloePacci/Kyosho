0d8670a79a1759a9539cee36fc27c40a5d999dc9
DESKTOP-G7VMHJI\Alejandro <alexcpesp@gmail.com>
2022-05-10 01:45:11 +0200

    cambios ligeros del codigo

--- kyosho/defines.h
+++ kyosho/defines.h
@@ -1,13 +1,26 @@
 #ifndef DEFINES
 #define DEFINES
+//#define USE_CAMERA
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
 #include "Arduino.h"
 #include "driver/mcpwm.h"
 
-
-
-
 #define PWM_READ_PIN 13
+#define PWM_WRITE_PIN 12
+
+
+#ifdef USE_CAMERA
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "other_code/CameraWebServer/camera_pins.h"
+const char* ssid = "AloePacci";
+const char* password = "alopako99";
+WiFiServer server(80);
+#endif
+
 
 void reader(void *pvParameters);
+void start_camera();
+void configure_pwm();
 #endif
 
